ba03427d3b29fb5d2b4a37f36354fc1074752aa0
Dragos Ionita <dragos.0x55@gmail.com>
2026-05-09 16:52:51 +0300

    add Generic instance

--- src/Toxiproxy.hs
+++ src/Toxiproxy.hs
@@ -85,7 +85,7 @@ instance ToJSON   ToxicName
 -- | The version of the Toxiproxy server. This library is fully supported by any version
 --   greater or equal to 2.1.3.
 newtype Version = Version Text
-  deriving (Show, Eq, MimeUnrender PlainText)
+  deriving (Show, Eq, MimeUnrender PlainText, Generic)
 
 instance FromJSON Version where
   parseJSON = genericParseJSON defaultOptions
